420c090bb976d150644f9636ebc2bcb30a3b2d28
Aleksander Spyra <a.spyra809@gmail.com>
2023-12-18 23:30:03 +0100

    Working on motor-buzzing. PPM receiving done, PWM driving sort-of done

--- development/ppm_receiving/ppm_receiving.ino
+++ development/ppm_receiving/ppm_receiving.ino
@@ -1,3 +1,4 @@
+#include <TimerFreeTone.h>
 // #define rcIN 2
 // #define hasLED false
 // #define redLED 0
@@ -6,6 +7,7 @@
 // SOFTPWM_DEFINE_CHANNEL_INVERT(1, DDRC, PORTC, PORTC3);  //Arduino pin 4 CpFET
 // SOFTPWM_DEFINE_CHANNEL(2, DDRB, PORTB, PORTB0);  //Arduino pin 5   CnFET
 // SOFTPWM_DEFINE_CHANNEL(3, DDRD, PORTD, PORTD5);  //Arduino pin 13  AnFET
+#define noDrive
 
 #define rcIN 2
 #define revH true
@@ -34,47 +36,20 @@ void pin_setup(){
   digitalWrite(pinCL, revL);
   //ppm input
   pinMode(rcIN, INPUT_PULLUP);
-}
-
-volatile bool ppm_flag = false;
-volatile uint16_t ppm_reading;
-
-volatile bool fwd = true;
-volatile uint8_t new_pwm = 0;
-
- //Iterrupt0 / Pin2 interrupt function. 
-ISR(INT0_vect){
-  //processing depends on rising/falling edge
-  if(digitalRead(rcIN)){
-    //rising edge - start timer1
-    TCNT1 = 0;
-    bitSet(TIFR, TOV1); //also reset overflow flag
-  }
-  else{
-    //falling edge
-    uint16_t temp = TCNT1;
-    bool overflow = bitRead(TIFR, TOV1);
-    if(overflow) Serial.println("Overflow error!");
-    else{
-      ppm_reading = temp;
-      ppm_flag = true;
-    }
+  while(1){
+    TimerFreeTone(pinCL, 4000, 500, 9);
+    delay(500);
   }
 }
 
-void ppm_setup(){
+void pwm_setup(){
   cli();
-  //rcIN interrupt setup. Pin 2 = INT0
-  //generate interrupt on any edge
-  bitClear(MCUCR, ISC01);
-  bitSet(MCUCR, ISC00);
-  //turn on INT0
-  bitSet(GICR, INT0);
-
-  //Timer1 setup: normal mode, no prescaler
-  TCCR1A = 0;
-  TCCR1B = 1;
-
+  //Timer2 setup: normal mode, no prescaler
+  //PWM frequency: 1kHz -> *100, 2kHz -> *011, 8kHz -> *010
+  TCCR2 = 0b00000010;
+  OCR2 = 240;
+  bitSet(TIMSK, OCIE2); //generate interrupt on compare match
+  bitSet(TIMSK, TOIE2); //generate interrupt on counter overflow
   // Serial.printf("TCCR1A: %x\n", TCCR1A); Serial.printf("TCCR1B: %x\n", TCCR1B); Serial.printf("TCNT1H: %x\n", TCNT1H);Serial.printf("TCNT1L: %x\n", TCNT1L);
   // Serial.printf("OCR1AH: %x\n", OCR1AH); Serial.printf("OCR1AL: %x\n", OCR1AH); Serial.printf("OCR1AH: %x\n", OCR1BH); Serial.printf("OCR1AL: %x\n", OCR1BH);
   // Serial.printf("ICR1H: %x\n", ICR1H); Serial.printf("ICR1L: %x\n", ICR1L); Serial.printf("TIMSK: %x\n", TIMSK); Serial.printf("TIFR: %x\n", TIFR);
@@ -109,9 +84,63 @@ inline void pwm_H_off(){
   bitClear(PORTD, PIND4);
 }
 
+volatile bool ppm_flag = false;
+volatile uint16_t ppm_reading;
+volatile uint8_t tim1_ovf_cnt = 0;
+
+volatile bool fwd = true;
+volatile uint8_t new_pwm = 0;
+
+ //Iterrupt0 / Pin2 interrupt function. 
+ISR(INT0_vect){
+  //processing depends on rising/falling edge
+  if(digitalRead(rcIN)){
+    //rising edge - start timer1
+    TCNT1 = 0;
+    tim1_ovf_cnt = 0;//bitSet(TIFR, TOV1); //also reset overflow flag
+  }
+  else{
+    //falling edge
+    uint16_t temp = TCNT1;
+    bool overflow = tim1_ovf_cnt > 0;//bitRead(TIFR, TOV1);
+    if(overflow) Serial.println("Overflow error!");
+    else{
+      ppm_reading = temp;
+      ppm_flag = true;
+    }
+  }
+}
+
+void ppm_setup(){
+  cli();
+  //rcIN interrupt setup. Pin 2 = INT0
+  //generate interrupt on any edge
+  bitClear(MCUCR, ISC01);
+  bitSet(MCUCR, ISC00);
+  //turn on INT0
+  bitSet(GICR, INT0);
+
+  //Timer1 setup: normal mode, no prescaler
+  TCCR1A = 0;
+  TCCR1B = 1;
+  TIMSK |= 1<<TOIE1; //interrupt on overflow enabled
+  // Serial.printf("TCCR1A: %x\n", TCCR1A); Serial.printf("TCCR1B: %x\n", TCCR1B); Serial.printf("TCNT1H: %x\n", TCNT1H);Serial.printf("TCNT1L: %x\n", TCNT1L);
+  // Serial.printf("OCR1AH: %x\n", OCR1AH); Serial.printf("OCR1AL: %x\n", OCR1AH); Serial.printf("OCR1AH: %x\n", OCR1BH); Serial.printf("OCR1AL: %x\n", OCR1BH);
+  // Serial.printf("ICR1H: %x\n", ICR1H); Serial.printf("ICR1L: %x\n", ICR1L); Serial.printf("TIMSK: %x\n", TIMSK); Serial.printf("TIFR: %x\n", TIFR);
+  sei();
+}
+
+ISR(TIMER1_OVF_vect){
+  tim1_ovf_cnt += 1;
+  if(tim1_ovf_cnt > 4)
+    Serial.println("TIM1 OVF");
+}
+
  //Tnterrupt timer 2 compare match -> turn on mosfets
 ISR(TIMER2_COMP_vect){
+#ifndef noDrive
   pwm_L_on();
+#endif
 }
 
  //Iterrupt timer 2 overflow -> turn off mosfets
@@ -127,7 +156,9 @@ ISR(TIMER2_OVF_vect){
     
     //14500-34000
     if(ppm < 14500){
+#ifndef noDrive
       pwm_L_on();
+#endif
       bitClear(TIMSK, OCIE2); //DON'T generate interrupt on compare match
     }
     else if(ppm > 34000){
@@ -141,23 +172,11 @@ ISR(TIMER2_OVF_vect){
     sei();
     Serial.printf("PPM: %u %u\n", ppm, OCR2);
   }
-  if(ppm >= 14500)
+  if(ppm >= 14500){
+#ifndef noDrive
     pwm_L_off();
-  
-}
-
-void pwm_setup(){
-  cli();
-  //Timer2 setup: normal mode, no prescaler
-  //PWM frequency: 1kHz -> *100, 2kHz -> *011, 8kHz -> *010
-  TCCR2 = 0b00000010;
-  OCR2 = 240;
-  bitSet(TIMSK, OCIE2); //generate interrupt on compare match
-  bitSet(TIMSK, TOIE2); //generate interrupt on counter overflow
-  // Serial.printf("TCCR1A: %x\n", TCCR1A); Serial.printf("TCCR1B: %x\n", TCCR1B); Serial.printf("TCNT1H: %x\n", TCNT1H);Serial.printf("TCNT1L: %x\n", TCNT1L);
-  // Serial.printf("OCR1AH: %x\n", OCR1AH); Serial.printf("OCR1AL: %x\n", OCR1AH); Serial.printf("OCR1AH: %x\n", OCR1BH); Serial.printf("OCR1AL: %x\n", OCR1BH);
-  // Serial.printf("ICR1H: %x\n", ICR1H); Serial.printf("ICR1L: %x\n", ICR1L); Serial.printf("TIMSK: %x\n", TIMSK); Serial.printf("TIFR: %x\n", TIFR);
-  sei();
+#endif
+  }
 }
 
 void setup(){
